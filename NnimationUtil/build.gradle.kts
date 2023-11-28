e938cb3e1d1bc4ebf1c98e7f6cdb2e411e45351c
neander <b3xlon9@gmail.com>
2023-11-28 17:05:19 +0900

    change gradle version... 8.0 → 7.4

--- NnimationUtil/build.gradle.kts
+++ NnimationUtil/build.gradle.kts
@@ -49,7 +49,7 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.8.0")
 }
 
 group="com.github.b3lon9"
