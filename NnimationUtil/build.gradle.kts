f5837fef609a11eea768e3077fb8b4d59b488da5
neander <b3xlon9@gmail.com>
2023-11-28 16:26:46 +0900

    jitpack 연동

--- NnimationUtil/build.gradle.kts
+++ NnimationUtil/build.gradle.kts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Neander, Cave-Dwellers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -33,11 +49,7 @@ android {
 }
 
 dependencies {
-
-    //implementation("androidx.core:core-ktx:1.9.0")
-    //implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
-    // testImplementation("junit:junit:4.13.2")
-    // androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    // androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+group="com.github.b3lon9"
