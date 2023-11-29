048f1e9dbd42d025833d05f9de05c3cecb35ed1d
neander <b3xlon9@gmail.com>
2023-11-29 16:35:36 +0900

    숨막혀 yml

--- NnimationUtil/build.gradle.kts
+++ NnimationUtil/build.gradle.kts
@@ -48,7 +48,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.b3lon9"
             artifactId = "NnimationUtil"
-            version = "1.2.3"
+            version = "1.2.4"
 
             afterEvaluate {
                 from(components["release"])
